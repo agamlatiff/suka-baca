d6d86b48a0e8c363b7ea80bb0dcd303b9f90c488
agamlatiff <agam.latiff@gmail.com>
2025-12-22 20:50:46 +0700

    Fix: Have error with wrong import section

--- app/Filament/Resources/Books/Schemas/BookForm.php
+++ app/Filament/Resources/Books/Schemas/BookForm.php
@@ -2,10 +2,10 @@
 
 namespace App\Filament\Resources\Books\Schemas;
 
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class BookForm
